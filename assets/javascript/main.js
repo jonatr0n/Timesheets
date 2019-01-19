52d6d1655a3cfba453693d80161a71b7bac32a43
dmar <dan.britain.marshall@gmail.com>
2019-01-19 10:32:45 -0600

    added firebase api

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA6GpaXKRQ6JiwqeLGL7H16Z3BqAUR-yOo",
+    authDomain: "logicproject-a9968.firebaseapp.com",
+    databaseURL: "https://logicproject-a9968.firebaseio.com",
+    projectId: "logicproject-a9968",
+    storageBucket: "logicproject-a9968.appspot.com",
+    messagingSenderId: "731850213115"
+  };
+  firebase.initializeApp(config);
