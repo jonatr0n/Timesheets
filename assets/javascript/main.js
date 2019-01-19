37a30e1c61f6f3f1bc7e21be51079d517cbfc29d
crcass <ccass@live.com>
2019-01-19 13:17:23 -0600

    added link to firebase

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,13 @@
 // Initialize Firebase
-// var config = {
-//   apiKey: "AIzaSyA6GpaXKRQ6JiwqeLGL7H16Z3BqAUR-yOo",
-//   authDomain: "logicproject-a9968.firebaseapp.com",
-//   databaseURL: "https://logicproject-a9968.firebaseio.com",
-//   projectId: "logicproject-a9968",
-//   storageBucket: "logicproject-a9968.appspot.com",
-//   messagingSenderId: "731850213115"
-// };
-// firebase.initializeApp(config);
+ var config = {
+   apiKey: "AIzaSyA6GpaXKRQ6JiwqeLGL7H16Z3BqAUR-yOo",
+   authDomain: "logicproject-a9968.firebaseapp.com",
+   databaseURL: "https://logicproject-a9968.firebaseio.com",
+   projectId: "logicproject-a9968",
+   storageBucket: "logicproject-a9968.appspot.com",
+   messagingSenderId: "731850213115"
+ };
+ firebase.initializeApp(config);
 
 $("button").on("click", function (event) {
   event.preventDefault();
